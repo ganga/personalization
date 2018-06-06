7fac6c2e4ded559e26e2113f2da9d7f6c2557994
Naga Gangadhar Reddy <nreddy@castlighthealth.com>
2018-06-06 12:50:08 +0530

    initialize spring boot application

---
+++ src/main/java/com/personalization/personalization/PersonalizationApplication.java
@@ -0,0 +1,12 @@
+package com.personalization.personalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalizationApplication.class, args);
+	}
+}
